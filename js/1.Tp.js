bd8990d8e2b39e742906267019d1d6c16d91ad43
Marcos <marcosvignoli19@gmail.com>
2022-07-13 16:24:24 -0300

    terminado el ejercicio 1

---
+++ js/1.Tp.js
@@ -0,0 +1,9 @@
+//punto 1
+let numero = parseInt(prompt('ingrese la edad'));
+console.log(numero);
+
+if(numero >= 18){
+    document.write('Ya puede conducir');
+}else{
+    document.write('no puede conducir');
+}
