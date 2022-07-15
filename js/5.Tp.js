22e29fca13ade7a1bbe0271809528e153a0cdddb
Marcos <marcosvignoli19@gmail.com>
2022-07-15 11:53:49 -0300

    terminado el punto 5

--- js/5.Tp.js
+++ js/5.Tp.js
@@ -24,8 +24,80 @@ if(dni >= 0 && dni <= 99999999){
         alert('W');
         break;
     case 3:
-        document.write('A');
-        alert('A');
+        document.write('G');
+        alert('G');
+        break;
+    case 4:
+        document.write('M');
+        alert('M');
+        break;
+    case 5:
+        document.write('Y');
+        alert('Y');
+        break;
+    case 6:
+        document.write('F');
+        alert('F');
+        break;
+    case 7:
+        document.write('P');
+        alert('P');
+        break;
+    case 8:
+        document.write('D');
+        alert('D');
+        break;
+    case 9:
+        document.write('X');
+        alert('X');
+        break;
+    case 10:
+        document.write('B');
+        alert('B');
+        break;
+    case 11:
+        document.write('N');
+        alert('N');
+        break;
+    case 12:
+        document.write('J');
+        alert('J');
+        break;
+    case 13:
+        document.write('Z');
+        alert('Z');
+        break;
+    case 14:
+        document.write('S');
+        alert('S');
+        break;
+    case 15:
+        document.write('Q');
+        alert('Q');
+        break;
+    case 16:
+        document.write('V');
+        alert('V');
+        break;
+    case 17:
+        document.write('H');
+        alert('H');
+        break;
+    case 18:
+        document.write('L');
+        alert('L');
+        break;
+    case 19:
+        document.write('C');
+        alert('C');
+        break;
+    case 20:
+        document.write('K');
+        alert('K');
+        break;
+    case 21:
+        document.write('E');
+        alert('E');
         break;
         default: 
         alert('valor erroneo');
